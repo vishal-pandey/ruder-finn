12b66dd1a122c2b436277c523bd87c15c28f0522
VISHAL PANDEY <contact@vishalpandey.xyz>
2020-03-24 00:47:20 +0530

    Updates

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 window.onload = ()=>{
 
+
 	var myFullpage = new fullpage('#fullpage', {
 		sectionsColor: ['#f2f2f2', '#f2f2f2', '#f2f2f2', '#f2f2f2', '#f2f2f2', '#f2f2f2', '#f2f2f2', '#f2f2f2', '#f2f2f2', '#f2f2f2', ],
 		anchors:['firstPage', 'secondPage', 'thirdPage', 'forthPage', 'fifthPage', 'sixthPage', 'seventhPage', 'eighthPage', 'ninethPage', 'tenthPage'],
@@ -22,6 +23,18 @@ window.onload = ()=>{
 	document.querySelector(".close-menu").addEventListener('click', ()=>{
 		document.querySelector(".full-page-menu").style.display = "none"
 	})
+
+
+
+	var spirals = document.querySelectorAll('.spiral')
+
+	for (var spiral of spirals){
+		spiral.style.left = spiral.dataset.left
+		spiral.style.right = spiral.dataset.right
+		spiral.style.top = spiral.dataset.top
+		spiral.style.bottom = spiral.dataset.bottom
+
+	}
 
 
 
